9af3531d9780500e1ba8b18cd31bce5a2b000956
Ahmad A'fif <ahmadafif.personal@gmail.com>
2022-02-24 04:08:18 +0800

    Update engine.py

--- src/api/core/engine.py
+++ src/api/core/engine.py
@@ -1,158 +1,42 @@
-from queue import Queue
-from time import time
-from uuid import uuid4
+import queue
 
-from dnstwist import Fuzzer, Scanner, UrlParser
-
-try:
-    import idna.codec
-except ImportError:
-    pass
-
-THREADS = 10
-SESSION_TTL = 300
-SESSION_MAX = 20
-
-DICTIONARY = (
-    "auth",
-    "account",
-    "confirm",
-    "connect",
-    "enroll",
-    "http",
-    "https",
-    "info",
-    "login",
-    "mail",
-    "my",
-    "online",
-    "payment",
-    "portal",
-    "recovery",
-    "register",
-    "ssl",
-    "safe",
-    "secure",
-    "signin",
-    "signup",
-    "support",
-    "update",
-    "user",
-    "verify",
-    "verification",
-    "web",
-    "www",
-)
-TLD_DICTIONARY = (
-    "com",
-    "net",
-    "org",
-    "info",
-    "cn",
-    "co",
-    "eu",
-    "de",
-    "uk",
-    "pw",
-    "ga",
-    "gq",
-    "tk",
-    "ml",
-    "cf",
-    "app",
-    "biz",
-    "top",
-    "xyz",
-    "online",
-    "site",
-    "live",
-)
+import dnstwist
 
 
-class Session:
-    def __init__(self, url, nameserver=None, thread_count=THREADS):
-        self.id = str(uuid4())
-        self.timestamp = int(time())
-        self.url = UrlParser(url)
-        self.nameserver = nameserver
-        self.thread_count = thread_count
-        self.jobs = Queue()
-        self.threads = []
-        fuzz = Fuzzer(
-            self.url.domain, dictionary=DICTIONARY, tld_dictionary=TLD_DICTIONARY
-        )
-        fuzz.generate()
-        self.permutations = fuzz.permutations()
+def engine(domain):
+    url = dnstwist.UrlParser(domain)
+    fuzz = dnstwist.Fuzzer(domain)
+    fuzz.generate()
 
-    def scan(self):
-        for domain in self.permutations:
-            self.jobs.put(domain)
-        for _ in range(self.thread_count):
-            worker = Scanner(self.jobs)
-            worker.setDaemon(True)
-            worker.option_extdns = True
-            worker.option_geoip = True
-            if self.nameserver:
-                worker.nameservers = [self.nameserver]
-            worker.start()
-            self.threads.append(worker)
+    jobs = queue.Queue()
+    for j in fuzz.domains:
+        jobs.put(j)
 
-    def stop(self):
-        self.jobs.queue.clear()
-        for worker in self.threads:
-            worker.stop()
-            worker.join()
-        self.threads.clear()
+    global threads
+    threads = []
 
-    def domains(self):
-        domains = [x for x in self.permutations.copy() if x.is_registered()]
+    for i in range(dnstwist.THREAD_COUNT_DEFAULT * 30):
+        worker = dnstwist.Scanner(jobs)
+        worker.setDaemon(True)
+        worker.uri_scheme = url.scheme
+        worker.uri_path = url.path
+        worker.uri_query = url.query
+        worker.option_extdns = True
+        worker.nameservers = ["1.1.1.1", "8.8.8.8"]
+        worker.option_geoip = True
 
-        def _idna(item):
-            try:
-                item["domain"] = item["domain"].encode().decode("idna")
-            except Exception:
-                pass
-            return item
+        worker.domain_orig = url.domain
 
-        return list(map(_idna, domains))
+        worker.start()
+        threads.append(worker)
 
-    def status(self):
-        if self.jobs.empty():
-            self.stop()
-        total = len(self.permutations)
-        remaining = max(self.jobs.qsize(), len(self.threads))
-        complete = total - remaining
-        registered = sum([1 for x in self.permutations if x.is_registered()])
-        return {
-            "id": self.id,
-            "timestamp": self.timestamp,
-            "url": self.url.full_uri(),
-            "total": total,
-            "complete": complete,
-            "remaining": remaining,
-            "registered": registered,
-        }
+    worker = dnstwist.Scanner(jobs)
+    worker.setDaemon(True)
+    worker.start()
+    worker.join()
 
-    def csv(self):
-        csv = ["fuzzer,domain,dns_a,dns_aaaa,dns_ns,dns_mx,geoip"]
-        for domain in list([x for x in self.permutations if x.is_registered()]):
-            csv.append(
-                ",".join(
-                    [
-                        domain.get("fuzzer"),
-                        domain.get("domain"),
-                        domain.get("dns_a", [""])[0],
-                        domain.get("dns_aaaa", [""])[0],
-                        domain.get("dns_ns", [""])[0],
-                        domain.get("dns_mx", [""])[0],
-                        domain.get("geoip", ""),
-                    ]
-                )
-            )
-        return "\n".join(csv)
+    domains = fuzz.permutations(registered=True)
 
-    def json(self):
-        return [x for x in self.permutations if x.is_registered()]
+    output = dnstwist.create_csv(domains)
 
-    def list(self):
-        return "\n".join([x.get("domain") for x in self.permutations])
+    return output
