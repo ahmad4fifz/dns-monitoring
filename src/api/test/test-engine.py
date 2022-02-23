954f14ecae461c4ed14510d8b4844e9c5db1ca85
Ahmad A'fif <ahmadafif.personal@gmail.com>
2022-02-24 04:17:45 +0800

    Update test-engine.py

--- src/api/test/test-engine.py
+++ src/api/test/test-engine.py
@@ -1,6 +1,8 @@
 import argparse
-from api.core.engine import 
+from api.core.engine import dnx
 
 parser = argparse.ArgumentParser(description="Test for engine.py")
-parser.add_argument("string", help="keyword(s) to query")
+parser.add_argument("string", help="domain to query")
 args = parser.parse_args()
+
+print(dnx(args.string))
