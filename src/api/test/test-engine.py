f18f33dc34221f6f84260735d0f3ff78dbe403ff
Ahmad A'fif <ahmadafif.personal@gmail.com>
2022-02-24 04:13:56 +0800

    Create test-engine.py

---
+++ src/api/test/test-engine.py
@@ -0,0 +1,6 @@
+import argparse
+from api.core.engine import 
+
+parser = argparse.ArgumentParser(description="Test for engine.py")
+parser.add_argument("string", help="keyword(s) to query")
+args = parser.parse_args()
